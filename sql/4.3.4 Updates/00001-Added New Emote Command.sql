99e23b1ccc15daabf7f3578258d857d87725e316
Roberto Andres Roeschmann Ruiz <icerobert@clandestino.cl>
2012-06-07 16:48:06 +0200

    Fix Error In Last Commit

--- sql/4.3.4 Updates/00001-Added New Emote Command.sql
+++ sql/4.3.4 Updates/00001-Added New Emote Command.sql
@@ -1,3 +1,1 @@
-DELETE FROM `spell_script_names` WHERE `spell_id` = (13165);
-INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES
-(13165, 'spell_hun_aspect_of_the_hawk');
+INSERT INTO `command` VALUES ("npc addemote", 3, "Syntax: .npc addemote #emoteid\n Add #emoteid to the selected creature permanently.");
